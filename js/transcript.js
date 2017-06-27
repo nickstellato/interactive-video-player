ab30d58f42aa27c793cdd58ec0128ef441c485f2
Nick Stellato <nick@nickstellato.com>
2017-06-26 22:21:07 -0500

    Continued working on highlighting the appropriate sentence.

--- js/transcript.js
+++ js/transcript.js
@@ -1,5 +1,28 @@
 (function(){
+
     const video = document.querySelector('video');
-    console.log(video);
-    console.log(video.currentTime);
+    const transcript = document.querySelector('.transcript');
+    const transcriptSections = transcript.querySelectorAll('span');
+
+    video.addEventListener('timeupdate', () => {
+        for (let i = 0; i < transcriptSections.length; i += 1) {
+            let section = transcriptSections.item(i);
+            let nextSection = section.nextElementSibling;
+
+            console.log(nextSection);
+
+            if (section.dataset.time < video.currentTime) {
+                section.className = 'highlight';
+            }
+
+            if (nextSection.dataset.time < video.currentTime) {
+                section.className = '';
+            }
+
+            // if (section.dataset.time < video.currentTime) {
+            //     section.className = 'highlight';
+            // }
+        }
+    });
+
 })();
